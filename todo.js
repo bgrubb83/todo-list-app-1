679763ecf866ed090b0905f635e1e5fd205dfa56
Barry Grubb <bgrubb83@gmail.com>
2020-01-31 16:32:39 +0000

    Render newly added todo list items to the page.

--- todo.js
+++ todo.js
@@ -2,8 +2,39 @@
 let todoList;
 
 /* Functions */
+function clearTodoList() {
+    const todoListContainer = document.querySelector("#todo-list-container");
+    todoListContainer.innerHTML = `<section id="todo-list-container"></section>`;
+}
+
+function renderTodoList() {
+    // Clear any existing todo list items
+    clearTodoList();
+
+    // Create HTML elements
+    const todoListContainer = document.querySelector("#todo-list-container");
+    const finalTodoListHTML = document.createElement("section");
+
+    todoList.forEach((todoListItem) => {
+        const todoListItemHTML = `
+            <section class="todo-list-item-container">
+                <h1 data-timestamp="${todoListItem.timestamp}">${todoListItem.text}</h1>
+                <section class="todo-list-item-delete">
+                X
+                </section>
+            </section>`;
+        finalTodoListHTML.innerHTML += (todoListItemHTML);
+    });
+
+    // Apppend the todo list items
+    todoListContainer.appendChild(finalTodoListHTML);
+
+    // Hook up delete buttons
+    hookupDeleteButtons();
+}
+
 function removeFromArrayByIndex(array, index) {
-    return array.slice(0, index).concat(array.slice(index+1, array.length));
+    return array.slice(0, index).concat(array.slice(index + 1, array.length));
 }
 
 function deleteTodoListItem(todoListItemTimestamp) {
@@ -34,40 +65,14 @@ function handleAdd(event) {
     const todoListInputBox = document.querySelector('#todo-list-input');
     const newTodoListItemText = todoListInputBox.value;
     if (newTodoListItemText) {
-        todoList.push({text: newTodoListItemText, timestamp: Date.now().toString()});
+        todoList.push({ text: newTodoListItemText, timestamp: Date.now().toString() });
+        renderTodoList();
     }
+    const form = event.target.parentElement;
+    form.reset();
 }
 
-/* Wait for DOM content to load */
-document.addEventListener('DOMContentLoaded', () => {
-    // Create todo list items
-    todoList = [
-    {text: 'Wash the car', timestamp: '1580419503630'},
-    {text: 'Feed the cat', timestamp: '1580419538130'},
-    {text: 'Go to work', timestamp: '1580419538140'},
-    {text: 'Eat pizza', timestamp: '1580419538150'},
-    {text: 'Learn to fly', timestamp: '1580419538160'},
-    {text: 'Read a book', timestamp: '1580419538170'},
-    ];
-
-    // Create HTML elements
-    const todoListContainer = document.querySelector("#todo-list-container");
-    const finalTodoListHTML = document.createElement("section");
-
-    todoList.forEach((todoListItem) => {
-        const todoListItemHTML = `
-        <section class="todo-list-item-container">
-            <h1 data-timestamp="${todoListItem.timestamp}">${todoListItem.text}</h1>
-            <section class="todo-list-item-delete">
-            X
-            </section>
-        </section>`;
-        finalTodoListHTML.innerHTML += (todoListItemHTML);
-    });
-
-    // Apppend the todo list items
-    todoListContainer.appendChild(finalTodoListHTML);
-
+function hookupDeleteButtons() {
     // Get all of the delete buttons
     const todoListItemDeleteIcons = document.querySelectorAll(".todo-list-item-delete")
 
@@ -75,9 +80,25 @@ document.addEventListener('DOMContentLoaded', () => {
     todoListItemDeleteIcons.forEach((el) => {
         el.addEventListener("click", handleDelete);
     });
+}
+
+/* Wait for DOM content to load */
+document.addEventListener('DOMContentLoaded', () => {
+    // Create todo list items
+    todoList = [
+        { text: 'Wash the car', timestamp: '1580419503630' },
+        { text: 'Feed the cat', timestamp: '1580419538130' },
+        { text: 'Go to work', timestamp: '1580419538140' },
+        { text: 'Eat pizza', timestamp: '1580419538150' },
+        { text: 'Learn to fly', timestamp: '1580419538160' },
+        { text: 'Read a book', timestamp: '1580419538170' },
+    ];
+
+    renderTodoList();
+    hookupDeleteButtons();
 
     // Hook up the input box and add button
     const todoListAddButton = document.querySelector('#todo-list-add-button');
     todoListAddButton.addEventListener("click", handleAdd);
-  })
+})
 
